b9b5f0af84a4a35eb691142a6cd6d15f4268f53a
Dustin Schoenbrun <dustin.schoenbrun@netapp.com>
2014-06-23 15:31:41 -0400

    Added positive functional tests for access groups
    Added tests for the CRUD operations for Access Groups in Manila.

--- tempest/api/share/test_access_groups.py
+++ tempest/api/share/test_access_groups.py
@@ -39,37 +39,72 @@ class AccessGroupsNFSTest(base.BaseSharesTest):
 
     @test.attr(type=['gate'])
     def test_create_access_group(self):
-        resp, resp_body = self.shares_client.create_access_group('ip')
-        self.assertEqual('200', resp['status'])
-        self.assertEqual('ip', resp_body['type'])
+        access_group_id, _ = self._create_and_validate_access_group(
+            name='test')
+        self.addCleanup(
+            self.shares_client.delete_access_group, access_group_id)
+
 
     @test.attr(type=['gate'])
     def test_modify_access_group(self):
-        resp, resp_body = self.shares_client.create_access_group('ip')
-        self.assertEqual('200', resp['status'])
-        self.assertEqual('ip', resp_body['type'])
-        access_group_id = resp_body['id']
-        resp, resp_body = self.shares_client.update_access_group(
-            access_group_id, description='modified', name='newName')
-        self.assertEqual('200', resp['status'])
-        self.assertEqual('modified', resp_body['description'])
-        self.assertEqual('newName', resp_body['name'])
+        access_group_id, _ = self._create_and_validate_access_group(
+            name='test')
+        self.addCleanup(self.shares_client.delete_access_group,
+                        access_group_id)
+        _ = self._update_and_validate_access_group(access_group_id,
+                                                   name='newName',
+                                                   desc='newDesc')
 
     @test.attr(type=['gate'])
     def test_list_all_access_groups(self):
-        raise NotImplementedError
+        # Create two access groups
+        access_group_id1, _ = self._create_and_validate_access_group(
+            name='test1')
+        self.addCleanup(
+            self.shares_client.delete_access_group, access_group_id1)
+        access_group_id2, _ = self._create_and_validate_access_group(
+            name='test2')
+        self.addCleanup(
+            self.shares_client.delete_access_group, access_group_id2)
+        # And then list them with details
+        resp, resp_body = self.shares_client.list_access_groups()
+        self.assertEqual('200', resp['status'])
+        #self.assertIn('test1', resp_body)
+        #self.assertIn('test2', resp_body)
 
     @test.attr(type=['gate'])
     def test_list_all_access_groups_detailed(self):
-        raise NotImplementedError
+        # Create two access groups
+        access_group_id1, _ = self._create_and_validate_access_group(
+            name='test1')
+        self.addCleanup(
+            self.shares_client.delete_access_group, access_group_id1)
+        access_group_id2, _ = self._create_and_validate_access_group(
+            name='test2')
+        self.addCleanup(
+            self.shares_client.delete_access_group, access_group_id2)
+        # And then list them with details
+        resp, resp_body = self.shares_client.list_access_groups_detail()
+        self.assertEqual('200', resp['status'])
+        #self.assertIn('entries', resp_body)
 
     @test.attr(type=['gate'])
     def test_show_access_group(self):
-        raise NotImplementedError
+        access_group_id, _ = self._create_and_validate_access_group(
+            name='test', desc='show_test')
+        self.addCleanup(
+            self.shares_client.delete_access_group, access_group_id)
+        resp, resp_body = self.shares_client.get_access_group(access_group_id)
+        self.assertEqual('200', resp['status'])
+        self.assertEqual('test', resp_body['name'])
+        self.assertEqual('show_test', resp_body['description'])
+        self.assertEqual(access_group_id, resp_body['id'])
 
     @test.attr(type=['gate'])
     def test_delete_access_group(self):
-        raise NotImplementedError
+        access_group_id, _ = self._create_and_validate_access_group()
+        resp, body = self.shares_client.delete_access_group(access_group_id)
+        self.assertEqual('202', resp['status'])
 
     @test.attr(type=['gate'])
     def test_add_entry_to_access_group(self):
@@ -79,7 +114,31 @@ class AccessGroupsNFSTest(base.BaseSharesTest):
     def test_remove_entry_from_access_group(self):
         raise NotImplementedError
 
+    def _create_and_validate_access_group(self, name=None, desc=None):
+        resp, resp_body = self.shares_client.create_access_group('ip',
+                                                                 name=name,
+                                                                 desc=desc)
+        self.assertEqual('200', resp['status'])
+        self.assertEqual('ip', resp_body['type'])
+        if name:
+            self.assertEqual(name, resp_body['name'])
+        if desc:
+            self.assertEqual(desc, resp_body['description'])
+        return resp_body['id'], resp_body
+
+    def _update_and_validate_access_group(self, access_group_id, name=None,
+                                          desc=None):
+        resp, resp_body = self.shares_client.update_access_group(
+            access_group_id, type='ip', description=desc, name=name)
+        self.assertEqual('200', resp['status'])
+        if name:
+            self.assertEqual(name, resp_body['name'])
+        if desc:
+            self.assertEqual(desc, resp_body['description'])
+        return resp_body
 
 class AccessGroupsCIFSTest(AccessGroupsNFSTest):
     """Covers Access Group Functionality as it's related to CIFS share types"""
     protocol = 'cifs'
+
+
